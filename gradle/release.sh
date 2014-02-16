6f50ea81f2f91a0249f6d62df95c968ff541b06e
Kenneth Endfinger <kaendfinger@gmail.com>
2014-02-16 18:46:50 -0500

    Added a release.sh script. I need to add a task in Gradle for it now :P

---
+++ gradle/release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+VERSION=`cat build.gradle | grep "version = \"" | awk '{gsub("version = ", "") ; gsub("\"","") ; print}'`
+echo "Releasing v${VERSION} of JPower"
+echo "Tagging Release"
+git tag v${VERSION} -m "JPower v${VERSION}"
+echo "Pushing Changes"
+git push origin master
+echo "Pushing Tags"
+git push --tags origin
+echo "Uploading to Sonatype"
+gradle upload
