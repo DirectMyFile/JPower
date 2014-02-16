931bcf1bcd2e0ef4093ee9e2aaf40877f1125719
Kenneth Endfinger <kaendfinger@gmail.com>
2014-02-16 18:49:07 -0500

    Force tag creation

--- gradle/release.sh
+++ gradle/release.sh
@@ -2,7 +2,7 @@
 VERSION=`cat build.gradle | grep "version = \"" | awk '{gsub("version = ", "") ; gsub("\"","") ; print}'`
 echo "Releasing v${VERSION} of JPower"
 echo "Tagging Release"
-git tag v${VERSION} -m "JPower v${VERSION}"
+git tag v${VERSION} -m "JPower v${VERSION}" -f
 echo "Pushing Changes"
 git push origin master
 echo "Pushing Tags"
