84a0d3becbad248720d3704e850a04af0a8c12a9
Kenneth Endfinger <kaendfinger@gmail.com>
2014-03-02 18:16:19 -0500

    Add Ignore message for IOUtilsTest.testResourceToString()

--- modules/Core/src/test/java/jpower/core/test/IOUtilsTest.java
+++ modules/Core/src/test/java/jpower/core/test/IOUtilsTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertNotNull;
 
 public class IOUtilsTest {
     @Test
-    @Ignore
+    @Ignore("Will fail in IntelliJ")
     public void testResourceToString() {
         String version = IOUtils.getResourceAsString(getClass(), "jpower/core/release");
         assertNotNull(version);
