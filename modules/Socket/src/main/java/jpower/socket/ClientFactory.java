ed5fd545dad73ab34226ff9b8024b220728997d0
Kenneth Endfinger <kaendfinger@gmail.com>
2014-03-03 23:09:14 -0500

    Remove Socket/settings.gradle and added a Socket ClientFactory

---
+++ modules/Socket/src/main/java/jpower/socket/ClientFactory.java
@@ -0,0 +1,31 @@
+package jpower.socket;
+
+import jpower.core.Factory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class ClientFactory implements Factory<Client> {
+
+    private final InetSocketAddress address;
+
+    public ClientFactory(String host, int port) {
+        address = new InetSocketAddress(host, port);
+    }
+
+    @Override
+    public Client create() {
+        Socket socket = new Socket();
+        try {
+            socket.connect(address);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            return new Client(socket);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
