80c8555e84c6edb0f6fbcd1a75493a2767f0e6bd
Logan Gorence <loganjohngorence@gmail.com>
2014-04-22 07:32:37 -0700

    Added a fibonacci sequence calculator to the PowerMath class, using a BigInteger array to return.

--- modules/Core/src/main/java/jpower/core/math/PowerMath.java
+++ modules/Core/src/main/java/jpower/core/math/PowerMath.java
@@ -1,6 +1,27 @@
 package jpower.core.math;
 
+import java.math.BigInteger;
+
 public class PowerMath
 {
 
+   /**
+    * Calculate the fibonacci sequence up to a certain digit.
+    *
+    * @param count Number of digits you want to calculate
+    * @return BigInteger array of numbers
+    */
+   public static BigInteger[] fibonacci(int count)
+   {
+      BigInteger[] digits = new BigInteger[count];
+      BigInteger last = new BigInteger("0");
+      BigInteger current = new BigInteger("1");
+      for (int i = 0; i < count; i++)
+      {
+         digits[i] = last;
+         current = last.add(current);
+         last = current.subtract(last);
+      }
+      return digits;
+   }
 }
