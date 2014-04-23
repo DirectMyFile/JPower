651708b43a4de0624ba760f6ed3d02c7bd95feba
Kenneth Endfinger <kaendfinger@gmail.com>
2014-04-23 10:33:02 -0400

    Get rid of the Groovy Module. All of this will be moved to Gutter.

--- modules/Groovy/src/main/groovy/jpower/groovy/JPowerExtension.groovy
+++
@@ -1,9 +0,0 @@
-package jpower.groovy
-
-import jpower.core.Wrapper
-
-class JPowerExtension {
-    static boolean asBoolean(Wrapper<?> wrapper) {
-        return wrapper.notNull && wrapper.get().asBoolean()
-    }
-}
