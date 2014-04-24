05995f354530b5b7c2df8a0fbeb97211881b5ee8
Kenneth Endfinger <kaendfinger@gmail.com>
2014-04-24 19:35:55 -0400

    Add useful CallUtils methods.

--- modules/Core/src/main/java/jpower/core/utils/CallUtils.java
+++ modules/Core/src/main/java/jpower/core/utils/CallUtils.java
@@ -2,6 +2,8 @@ package jpower.core.utils;
 
 import jpower.core.ActionThrowable;
 
+import java.util.function.Consumer;
+
 public class CallUtils
 {
    public static void callIgnoreExceptions(ActionThrowable action)
@@ -11,6 +13,28 @@ public class CallUtils
          action.run();
       } catch (Throwable ignored)
       {
+      }
+   }
+
+   public static void callPrintStackTrace(ActionThrowable action)
+   {
+      try
+      {
+         action.run();
+      } catch (Throwable e)
+      {
+         e.printStackTrace();
+      }
+   }
+
+   public static void tryCatch(ActionThrowable action, Consumer<Throwable> exceptionHandler)
+   {
+      try
+      {
+         action.run();
+      } catch (Throwable e)
+      {
+         exceptionHandler.accept(e);
       }
    }
 }
--- modules/Core/src/test/java/jpower/core/test/CallUtilsTest.java
+++ modules/Core/src/test/java/jpower/core/test/CallUtilsTest.java
@@ -1,10 +1,29 @@
 package jpower.core.test;
 
+import jpower.core.Wrapper;
 import jpower.core.utils.CallUtils;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@SuppressWarnings("ThrowableResultOfMethodCallIgnored")
 public class CallUtilsTest
 {
+   @Test
+   public void testTryCatch()
+   {
+      Wrapper<Throwable> caught = new Wrapper<>(null);
+      CallUtils.tryCatch(() -> {
+         throw new Exception("This should be caught");
+      }, caught::set);
+      CallUtils.tryCatch(() -> {
+         throw new Exception("LOL");
+      }, Throwable::printStackTrace);
+      assertNotNull(caught.get());
+      assertEquals("This should be caught", caught.get().getMessage());
+   }
+
    @Test
    public void testCallIgnoreExceptions()
    {
