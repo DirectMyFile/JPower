587c54a1edbb83e1f6dbe4a14c3bc6ca5b83418d
Kenneth Endfinger <kaendfinger@gmail.com>
2014-04-24 18:58:46 -0400

    ListUtils.forEach(List<T>, BiConsumer<T, Integer>) implemented and used.

--- modules/Core/src/main/java/jpower/core/utils/ListUtils.java
+++ modules/Core/src/main/java/jpower/core/utils/ListUtils.java
@@ -2,6 +2,7 @@ package jpower.core.utils;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BiConsumer;
 
 public class ListUtils
 {
@@ -18,14 +19,21 @@ public class ListUtils
    public static String join(List<String> inputs, String joinBy)
    {
       StringBuilder builder = new StringBuilder();
-      for (int i = 0; i < inputs.size(); i++)
-      {
+      forEach(inputs, (input, i) -> {
          if (i != 0 && i != inputs.size() - 1)
          {
             builder.append(joinBy);
          }
-         builder.append(inputs.get(i));
-      }
+         builder.append(input);
+      });
       return builder.toString();
+   }
+
+   public static <T> void forEach(List<T> that, BiConsumer<T, Integer> consumer)
+   {
+      for (int i = 0; i < that.size(); i++)
+      {
+         consumer.accept(that.get(i), i);
+      }
    }
 }
