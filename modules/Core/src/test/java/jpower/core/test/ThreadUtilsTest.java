4ae7b4fb4e3b0a84203f78ebfdec01e0b49875b4
Kenneth Endfinger <kaendfinger@gmail.com>
2014-04-24 19:25:44 -0400

    Added more tests for ThreadUtils

--- modules/Core/src/test/java/jpower/core/test/ThreadUtilsTest.java
+++ modules/Core/src/test/java/jpower/core/test/ThreadUtilsTest.java
@@ -1,5 +1,6 @@
 package jpower.core.test;
 
+import jpower.core.Wrapper;
 import jpower.core.utils.ArrayUtils;
 import jpower.core.utils.ThreadUtils;
 import org.junit.Test;
@@ -11,6 +12,26 @@ import static org.junit.Assert.assertTrue;
 
 public class ThreadUtilsTest
 {
+   @Test
+   public void testStartThread()
+   {
+      Wrapper<Boolean> value = new Wrapper<>(false);
+      Thread thread = ThreadUtils.start(() -> value.set(true));
+      assertFalse(thread.isDaemon());
+      ThreadUtils.sleep(40);
+      assertTrue(value.get());
+   }
+
+   @Test
+   public void testStartThreadDaemon()
+   {
+      Wrapper<Boolean> value = new Wrapper<>(false);
+      Thread thread = ThreadUtils.startDaemon(() -> value.set(true));
+      assertTrue(thread.isDaemon());
+      ThreadUtils.sleep(40);
+      assertTrue(value.get());
+   }
+
    @Test
    public void testGetAllThreads()
    {
