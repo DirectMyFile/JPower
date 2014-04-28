b32830dfece995a893fc5b7e7bdc6c7ab5b27867
Kenneth Endfinger <kaendfinger@gmail.com>
2014-04-28 18:42:09 -0400

    Add ServiceTest to the Core Test Suite.

--- modules/Core/src/test/java/jpower/core/test/CoreTestSuite.java
+++ modules/Core/src/test/java/jpower/core/test/CoreTestSuite.java
@@ -4,10 +4,25 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(
-        {AlphabetTest.class, ConditionTest.class, ConfigurationTest.class, FactoryTest.class, InternalTest.class,
-                IOUtilsTest.class, LoadTest.class, MultiMapTest.class, PowerLoaderTest.class, RandomTest.class,
-                ThreadUtilsTest.class, ByteUtilsTest.class, WorkerTest.class, GenericSnifferTest.class, OperatingSystemTest.class, CallUtilsTest.class})
+@Suite.SuiteClasses({
+        AlphabetTest.class,
+        ConditionTest.class,
+        ConfigurationTest.class,
+        FactoryTest.class,
+        InternalTest.class,
+        IOUtilsTest.class,
+        LoadTest.class,
+        MultiMapTest.class,
+        PowerLoaderTest.class,
+        RandomTest.class,
+        ThreadUtilsTest.class,
+        ByteUtilsTest.class,
+        WorkerTest.class,
+        GenericSnifferTest.class,
+        OperatingSystemTest.class,
+        CallUtilsTest.class,
+        ServiceTest.class
+})
 public class CoreTestSuite
 {
 }
