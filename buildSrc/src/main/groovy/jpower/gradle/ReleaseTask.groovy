e6d8f2d1578a954af6a228e176e0ba84159f9d3e
Kenneth Endfinger <kaendfinger@gmail.com>
2014-04-28 17:19:40 -0400

    Separate different release actions into blocks in the code.

--- buildSrc/src/main/groovy/jpower/gradle/ReleaseTask.groovy
+++ buildSrc/src/main/groovy/jpower/gradle/ReleaseTask.groovy
@@ -14,52 +14,68 @@ class ReleaseTask extends DefaultTask {
     @TaskAction
     void release() {
         def nextVersion = new Version(project.version as String).increment().toString()
-        if (pushRelease) {
-            logger.lifecycle "Pushing to Git Repository"
+
+        /* Actually Release */
+        ({
+            if (pushRelease)
+            {
+                logger.lifecycle "Pushing to Git Repository"
+                project.exec {
+                    commandLine "git", "push", "origin", "develop"
+                }
+            }
+
+            if (tagRelease)
+            {
+                logger.lifecycle "Creating Tag v${project.version}"
+                project.exec {
+                    commandLine "git", "tag", "v${project.version}"
+                }
+            }
+
+            if (rebaseMaster)
+            {
+                logger.lifecycle "Updating Master Branch"
+                project.exec {
+                    commandLine "git", "checkout", "master"
+                }
+                project.exec {
+                    commandLine "git", "rebase", "v${nextVersion}"
+                }
+                if (pushRelease)
+                {
+                    project.exec {
+                        commandLine "git", "push", "origin", "master"
+                    }
+                }
+                project.exec {
+                    commandLine "git", "checkout", "develop"
+                }
+            }
+
+            if (pushRelease)
+            {
+                logger.lifecycle "Pushing Tags to Git Repository"
+                project.exec {
+                    commandLine "git", "push", "--tags"
+                }
+            }
+        })()
+
+        /* Update to new version on develop branch */
+        ({
+            def propFile = project.file("gradle.properties")
+            propFile.text = propFile.text.replace("jpower.version=${project.version}", "jpower.version=${nextVersion}")
+            project.exec {
+                commandLine "git", "add", "gradle.properties"
+            }
+            project.exec {
+                commandLine "git", "commit", "-m", "Update Version to v${nextVersion}"
+            }
             project.exec {
                 commandLine "git", "push", "origin", "develop"
             }
-        }
-        if (tagRelease) {
-            logger.lifecycle "Creating Tag v${project.version}"
-            project.exec {
-                commandLine "git", "tag", "v${project.version}"
-            }
-        }
-        if (rebaseMaster) {
-            logger.lifecycle "Updating Master Branch"
-            project.exec {
-                commandLine "git", "checkout", "master"
-            }
-            project.exec {
-                commandLine "git", "rebase", "v${nextVersion}"
-            }
-            if (pushRelease) {
-                project.exec {
-                    commandLine "git", "push", "origin", "master"
-                }
-            }
-            project.exec {
-                commandLine "git", "checkout", "develop"
-            }
-        }
-        if (pushRelease) {
-            logger.lifecycle "Pushing Tags to Git Repository"
-            project.exec {
-                commandLine "git", "push", "--tags"
-            }
-        }
-        def propFile = project.file("gradle.properties")
-        propFile.text = propFile.text.replace("jpower.version=${project.version}", "jpower.version=${nextVersion}")
-        project.exec {
-            commandLine "git", "add", "gradle.properties"
-        }
-        project.exec {
-            commandLine "git", "commit", "-m", "Update Version to v${nextVersion}"
-        }
-        project.exec {
-            commandLine "git", "push", "origin", "develop"
-        }
+        })()
     }
 
     void git(Map<String, Boolean> options) {
