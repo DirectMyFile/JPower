f6e7c2790fc83b6e3fbb01bb4e7fb8144ec4a32b
Kenneth Endfinger <kaendfinger@gmail.com>
2014-04-29 19:35:26 -0400

    Added the @Frozen annotation.

---
+++ modules/Core/src/main/java/jpower/core/annotation/Frozen.java
@@ -0,0 +1,21 @@
+package jpower.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a Class or Package as being 'Frozen', as in it wont change until this annotation is removed.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.PACKAGE})
+public @interface Frozen
+{
+   /**
+    * The timestamp of being frozen
+    *
+    * @return timestamp
+    */
+   String value() default "";
+}
