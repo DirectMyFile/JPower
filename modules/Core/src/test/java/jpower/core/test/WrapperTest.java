a90010e376a46c1a91b8365469c5aa642717e7f9
Kenneth Endfinger <kaendfinger@gmail.com>
2014-04-28 20:21:18 -0400

    Simple WrapperTest

--- modules/Core/src/test/java/jpower/core/test/WrapperTest.java
+++ modules/Core/src/test/java/jpower/core/test/WrapperTest.java
@@ -1,6 +1,19 @@
 package jpower.core.test;
 
+import jpower.core.Wrapper;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("EqualsBetweenInconvertibleTypes")
 public class WrapperTest
 {
-
+   @Test
+   public void test() {
+      Wrapper<String> hello = new Wrapper<>("Hello");
+      assertEquals("Hello", hello.get());
+      assertEquals("Hello".hashCode(), hello.hashCode());
+      assertTrue(hello.equals("Hello"));
+   }
 }
