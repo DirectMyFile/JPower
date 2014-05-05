875f7fa5bae163c86b8e3ccde3bbfd8ee994bf14
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-05 10:39:57 -0400

    List Serialization Support

--- modules/JSON/src/main/java/jpower/json/serialization/JSONSerializer.java
+++ modules/JSON/src/main/java/jpower/json/serialization/JSONSerializer.java
@@ -6,6 +6,7 @@ import jpower.json.ObjectMapper;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 public class JSONSerializer
@@ -33,6 +34,26 @@ public class JSONSerializer
       }
    }
 
+   public String serialize(List<?> list) throws IOException
+   {
+      StringWriter writer = new StringWriter();
+      IndentPrinter out = new IndentPrinter(style.getIndention(), writer);
+      out.write('[');
+      out.println();
+      out.increment();
+
+      for (Object entry : list)
+      {
+         out.printIndent();
+         toJSON(entry, out);
+         out.println();
+      }
+      out.decrement();
+      out.write(']');
+      out.close();
+      return writer.toString();
+   }
+
    public String serialize(Map<?, ?> map) throws IOException
    {
       StringWriter writer = new StringWriter();
@@ -52,15 +73,7 @@ public class JSONSerializer
          out.write('"');
          out.write(':');
          out.write(' ');
-         if (value.getClass().isAssignableFrom(Integer.class)) {
-            out.print(toJSON((int) value));
-         } else if (value.getClass().isAssignableFrom(Long.class)) {
-            out.print(toJSON((long) value));
-         } else if (value.getClass().isAssignableFrom(String.class)) {
-            out.print(toJSON((String) value, style));
-         } else {
-            out.print(serialize(value));
-         }
+         toJSON(value, out);
          if (count != map.keySet().size())
          {
             out.write(',');
@@ -74,6 +87,23 @@ public class JSONSerializer
       out.write('}');
       out.close();
       return writer.toString();
+   }
+
+   public void toJSON(Object value, IndentPrinter out) throws IOException
+   {
+      if (value.getClass().isAssignableFrom(Integer.class))
+      {
+         out.print(toJSON((int) value));
+      } else if (value.getClass().isAssignableFrom(Long.class))
+      {
+         out.print(toJSON((long) value));
+      } else if (value.getClass().isAssignableFrom(String.class))
+      {
+         out.print(toJSON((String) value, style));
+      } else
+      {
+         out.print(serialize(value));
+      }
    }
 
    public static String toJSON(int number) {
