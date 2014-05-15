8696a09b74f4cd2d3dc8d66093de445fe850ad59
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-15 10:22:45 -0400

    JSON: Add Null Class

---
+++ modules/JSON/src/main/java/jpower/json/JSONNull.java
@@ -0,0 +1,14 @@
+package jpower.json;
+
+public class JSONNull
+{
+   public static JSONNull NULL = new JSONNull();
+
+   private JSONNull()
+   {}
+
+   @Override
+   public String toString() {
+      return "null";
+   }
+}
