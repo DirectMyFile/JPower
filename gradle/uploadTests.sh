b1a4cdd97689edcaf80e5032142cc6410f407fb8
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-25 18:38:36 -0400

    Travis CI: Attempt to fix again

--- gradle/uploadTests.sh
+++ gradle/uploadTests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git clone https://${UPLOAD_USERNAME}:${UPLOAD_PASSWORD}@github.com/DirectMyFile/tests.git .tmp/
 ORIGDIR=${PWD}
+./.tmp/copy.sh JPower ${PWD}/build/reports/allTests/
 cd .tmp/
-./copy.sh JPower ${PWD}/build/reports/allTests/
 git add .; git ls-files --deleted | xargs --no-run-if-empty git rm
 git commit -m "Update JPower Tests"
 git push -u origin gh-pages
