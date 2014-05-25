8be1e8d8d91ad140806730941ffe85af4d16fefb
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-25 16:44:06 -0400

    Core: GotoContext - Add test for Chaining

--- modules/Core/src/test/java/jpower/core/test/GotoContextTest.java
+++ modules/Core/src/test/java/jpower/core/test/GotoContextTest.java
@@ -17,4 +17,16 @@ public class GotoContextTest
       ctx.run("increment");
       assertEquals(1, number.get().intValue());
    }
+
+   @Test
+   public void testChaining()
+   {
+      GotoContext ctx = GotoContext.make();
+      Wrapper<Integer> number = Wrapper.of(0);
+      ctx.define("increment", () -> number.set(number.get() + 1));
+      ctx.define("decrement", () -> number.set(number.get() - 1));
+      ctx.run("increment")
+         .run("decrement");
+      assertEquals(0, number.get().intValue());
+   }
 }
