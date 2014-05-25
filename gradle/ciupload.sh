694f56a424b7cacb84265c5e3f9a38f51bdcc9dd
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-25 19:07:45 -0400

    Travis CI: Upload build/ to the build site

--- gradle/ciupload.sh
+++ gradle/ciupload.sh
@@ -5,13 +5,13 @@ git clone https://${UPLOAD_USERNAME}:${UPLOAD_PASSWORD}@github.com/DirectMyFile/
 
 ORIGDIR=${PWD}
 
-# Copy JavaDoc to the Storage
-./.tmp/copy.sh JPower/javadoc/ ${PWD}/build/javadoc/
-
-# Copy Test Results to the Storage
-./.tmp/copy.sh JPower/tests/ ${PWD}/build/reports/allTests/
+# Copy JPower Build Data
+./.tmp/copy.sh JPower ${PWD}/build/
 
 cd .tmp/
+
+rm -rf JPower/tmp
+
 git add .; git ls-files --deleted | xargs --no-run-if-empty git rm
 git commit -m "Update JPower Results"
 git push -u origin gh-pages
