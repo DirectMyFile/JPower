839e40853c3d1d24baff7e862c409cd648d010ae
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-25 18:58:33 -0400

    Travis CI: Publish to Sonatype

--- gradle/ciupload.sh
+++ gradle/ciupload.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+# Clone Build Storage
 git clone https://${UPLOAD_USERNAME}:${UPLOAD_PASSWORD}@github.com/DirectMyFile/build.git .tmp/
+
 ORIGDIR=${PWD}
+
+# Copy JavaDoc to the Storage
 ./.tmp/copy.sh JPower/javadoc/ ${PWD}/build/javadoc/
+
+# Copy Test Results to the Storage
 ./.tmp/copy.sh JPower/tests/ ${PWD}/build/reports/allTests/
+
 cd .tmp/
 git add .; git ls-files --deleted | xargs --no-run-if-empty git rm
 git commit -m "Update JPower Results"
