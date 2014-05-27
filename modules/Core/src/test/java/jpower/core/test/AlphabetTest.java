9e5db766ba322795d942504e787232cf2a467c05
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-26 20:51:29 -0400

    Gradle: Add More Alphabet Tests

--- modules/Core/src/test/java/jpower/core/test/AlphabetTest.java
+++ modules/Core/src/test/java/jpower/core/test/AlphabetTest.java
@@ -3,7 +3,7 @@ package jpower.core.test;
 import jpower.core.Alphabet;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class AlphabetTest
 {
@@ -25,5 +25,44 @@ public class AlphabetTest
       {
          assertFalse(Character.isUpperCase(letter));
       }
+   }
+
+   @Test
+   public void testLength()
+   {
+      assertEquals(26, Alphabet.length());
+   }
+
+   @Test(expected = IndexOutOfBoundsException.class)
+   public void testRangeSingleOutOfBounds()
+   {
+      Alphabet.getRange(-1);
+   }
+
+   @Test(expected = IndexOutOfBoundsException.class)
+   public void testRangeDoubleOutOfBounds()
+   {
+      Alphabet.getRange(-1, -5);
+   }
+
+   @Test
+   public void testRangeAll()
+   {
+      char[] range = Alphabet.getRange(0);
+      assertEquals(26, range.length);
+   }
+
+   @Test
+   public void testRangeSingle()
+   {
+      char[] range = Alphabet.getRange(25);
+      assertEquals(1, range.length);
+   }
+
+   @Test
+   public void testRangeDouble()
+   {
+      char[] range = Alphabet.getRange(0, 3);
+      assertEquals(3, range.length);
    }
 }
