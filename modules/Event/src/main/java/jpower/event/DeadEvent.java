15d887a98fcae009cec44a4733c2f68d52b71e44
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-28 15:43:55 -0400

    Event: DeadEvent - Add Documentation

--- modules/Event/src/main/java/jpower/event/DeadEvent.java
+++ modules/Event/src/main/java/jpower/event/DeadEvent.java
@@ -2,20 +2,38 @@ package jpower.event;
 
 import java.io.Serializable;
 
+/**
+ * Represents an Event that did not get handled by any handlers
+ */
 public class DeadEvent implements Serializable
 {
    private final Object event;
 
+   /**
+    * Creates a new DeadEvent
+    * 
+    * @param event event
+    */
    public DeadEvent(Object event)
    {
       this.event = event;
    }
 
+   /**
+    * Gets the Event Object
+    * 
+    * @return event object
+    */
    public Object getEvent()
    {
       return event;
    }
 
+   /**
+    * Gets the Event Type
+    * 
+    * @return event class type
+    */
    public Class<?> getType()
    {
       return event.getClass();
