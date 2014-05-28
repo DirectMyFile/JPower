a9a835c0c00d13dbdeb0c8a8842982b749e04558
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-28 17:25:39 -0400

    Update Gradle Wrapper and add HashCodeBuilder

---
+++ modules/Core/src/main/java/jpower/core/HashCodeBuilder.java
@@ -0,0 +1,44 @@
+package jpower.core;
+
+import jpower.core.reflect.FieldAccessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashCodeBuilder
+{
+   private List<String> includes = new ArrayList<>();
+
+   public static HashCodeBuilder create()
+   {
+      return new HashCodeBuilder();
+   }
+
+   public HashCodeBuilder include(String field)
+   {
+      includes.add(field);
+      return this;
+   }
+
+   public int hashCodeFor(Object obj)
+   {
+      FieldAccessor accessor = new FieldAccessor(obj);
+      int code = 0;
+      for (String fieldName : includes)
+      {
+         try
+         {
+            Object val = accessor.get(fieldName);
+            if (val != null)
+            {
+               code += 31 * val.hashCode();
+            }
+         }
+         catch (NoSuchFieldException | IllegalAccessException e)
+         {
+            throw new RuntimeException(e);
+         }
+      }
+      return code;
+   }
+}
