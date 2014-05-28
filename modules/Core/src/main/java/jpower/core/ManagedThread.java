52e1d4dff4db98c0918b982eb542863ce17b8fd0
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-28 11:39:31 -0400

    Core: Managed Thread - Work just like Thread for now

--- modules/Core/src/main/java/jpower/core/ManagedThread.java
+++ modules/Core/src/main/java/jpower/core/ManagedThread.java
@@ -2,7 +2,21 @@ package jpower.core;
 
 /**
  * A ManagedThread is a Thread that is optimized to allow easy stopping.
+ * 
+ * <p>First the Thread specifies the code to run</p>
  */
-public class ManagedThread
+public class ManagedThread extends Thread
 {
+   private Runnable action;
+   
+   public ManagedThread(Runnable action)
+   {
+     this.action = action;
+   }
+   
+   @Override
+   public void run()
+   {
+      action.run();
+   }
 }
