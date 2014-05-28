3577a6481f50a1dc1dd4e1a36478272b41bf4d3e
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-28 16:12:22 -0400

    Core: Service - Documentation

--- modules/Core/src/main/java/jpower/core/Service.java
+++ modules/Core/src/main/java/jpower/core/Service.java
@@ -1,11 +1,23 @@
 package jpower.core;
 
+/**
+ * A Service is a thing that can be started and stopped.
+ */
 public interface Service
 {
+   /**
+    * Starts the Service
+    */
    void start();
 
+   /**
+    * Stops the Service
+    */
    void stop();
 
+   /**
+    * Restarts the Service
+    */
    default void restart()
    {
       stop();
