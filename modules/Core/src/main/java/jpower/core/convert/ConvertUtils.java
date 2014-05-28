ea0664e821e8d159421795f7d319d03d57c44148
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-28 10:19:59 -0400

    Core: Remove jpower.core.convert

--- modules/Core/src/main/java/jpower/core/convert/ConvertUtils.java
+++
@@ -1,9 +0,0 @@
-package jpower.core.convert;
-
-public class ConvertUtils
-{
-   public static <T> T convert(Convertible object, Class<T> clazz)
-   {
-      return object.convert(clazz);
-   }
-}
--- modules/Core/src/main/java/jpower/core/convert/Convertible.java
+++
@@ -1,6 +0,0 @@
-package jpower.core.convert;
-
-public interface Convertible
-{
-   <T> T convert(Class<T> clazz);
-}
