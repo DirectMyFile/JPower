1c335bf76589bb96d6cbb5cd2ebfbf03b9b348a6
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-29 21:14:20 -0400

    Core: Add Documentation for Regular Expression Stuff

--- modules/Core/src/main/java/jpower/core/utils/RegularExpression.java
+++ modules/Core/src/main/java/jpower/core/utils/RegularExpression.java
@@ -5,18 +5,39 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Regular Expression Utilities
+ */
 public class RegularExpression
 {
+   /**
+    * Creates a String Scanner
+    *
+    * @param str input string
+    * @return string scanner
+    */
    public static StringScanner scanner(String str)
    {
       return new StringScanner(str);
    }
 
+   /**
+    * Checks if a String Matches a Pattern
+    * @param pattern pattern
+    * @param input input string
+    * @return true if it matches, otherwise false
+    */
    public static boolean matches(Pattern pattern, String input)
    {
       return pattern.matcher(input).matches();
    }
 
+   /**
+    * Gets the Capture Groups for the pattern again the input
+    * @param pattern pattern
+    * @param input input string
+    * @return array of capture groups
+    */
    public static String[] captures(Pattern pattern, String input)
    {
       List<String> captures = new ArrayList<>();
@@ -28,6 +49,12 @@ public class RegularExpression
       return captures.toArray(new String[captures.size()]);
    }
 
+   /**
+    * Gets the Capture Groups for the pattern again the input
+    * @param pattern pattern
+    * @param input input string
+    * @return array of capture groups
+    */
    public static String[] captures(String pattern, String input)
    {
       return captures(Pattern.compile(pattern), input);
--- modules/Core/src/main/java/jpower/core/utils/StringScanner.java
+++ modules/Core/src/main/java/jpower/core/utils/StringScanner.java
@@ -3,20 +3,38 @@ package jpower.core.utils;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+/**
+ * Scans a String for Patterns
+ */
 public class StringScanner
 {
    private Scanner scanner;
 
+   /**
+    * Creates a new String Scanner
+    *
+    * @param str input string
+    */
    public StringScanner(String str)
    {
       scanner = new Scanner(str);
    }
 
+   /**
+    * Find the next string matching the pattern
+    * @param pattern pattern
+    * @return matched
+    */
    public String scan(String pattern)
    {
       return scanner.next(pattern);
    }
 
+   /**
+    * Find the next string matching the pattern
+    * @param pattern pattern
+    * @return matched
+    */
    public String scan(Pattern pattern)
    {
       return scanner.next(pattern);
