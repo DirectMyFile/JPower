cd25c2c3664e34defa578fe8fdc176cc9a88f861
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-04 12:05:23 -0400

    Core: Documented OperatingSystem

--- modules/Core/src/main/java/jpower/core/OperatingSystem.java
+++ modules/Core/src/main/java/jpower/core/OperatingSystem.java
@@ -14,56 +14,102 @@ public final class OperatingSystem
       this.name = name.toLowerCase();
    }
 
+   /**
+    * Gets the Current Operating System
+    *
+    * @return Current OS
+    */
    public static OperatingSystem current()
    {
       return current;
    }
 
+   /**
+    * Gets the specified Operating System
+    * @param name OS Name
+    * @return Operating System
+    */
    public static OperatingSystem forName(String name)
    {
       return new OperatingSystem(name);
    }
 
+   /**
+    * Checks if the Current OS is Windows
+    * @return true if Windows, otherwise false
+    */
    public boolean isWindows()
    {
       return name.contains("win");
    }
 
+   /**
+    * Checks if the Current OS is Mac OSX
+    * @return true if Mac OSX, otherwise false
+    */
    public boolean isMac()
    {
       return name.contains("mac");
    }
 
+   /**
+    * Checks if the Current OS is Unix
+    * @return true if Unix, otherwise false
+    */
    public boolean isUnix()
    {
       return name.contains("nix") || name.contains("nux") || name.contains("aix");
    }
 
+   /**
+    * Checks if the Current OS is Solaris
+    * @return true if Solaris, otherwise false
+    */
    public boolean isSolaris()
    {
       return name.contains("sunos");
    }
 
+   /**
+    * Checks if the OS could not be determined
+    * @return true if not determined, otherwise false
+    */
    public boolean isUnknown()
    {
       return !(isUnix() || isSolaris() || isMac() || isWindows());
    }
 
+   /**
+    * Checks if the OS is Case Sensitive
+    * @return true if case sensitive, otherwise false
+    */
    public boolean isCaseSensitive()
    {
       return isUnix();
    }
 
+   /**
+    * Checks if the Operating System is the same as the Current Operating System
+    * @return true if the current os, otherwise false
+    */
    public boolean isCurrent()
    {
       return current().name.equals(name);
    }
 
+   /**
+    * Gets the OS Version
+    * @return OS Version if the instance is the Current OS, otherwise "unknown"
+    */
    public String version()
    {
       return isCurrent() ? System.getProperty("os.version") : "unknown";
    }
 
+   /**
+    * Gets the Line Separator for the OS
+    * @return Line Separator
+    */
    public String lineSeparator()
    {
       if (isUnix())
@@ -74,7 +120,7 @@ public final class OperatingSystem
       {
          return "\r\n";
       }
-      else if (isMac())
+      else if (isMac()) // TODO: Checkup on Mac OSX Line Separator
       {
          return "\r";
       }
@@ -84,6 +130,10 @@ public final class OperatingSystem
       }
    }
 
+   /**
+    * Gets the OS Name
+    * @return OS Name
+    */
    public String getName()
    {
       return name;
